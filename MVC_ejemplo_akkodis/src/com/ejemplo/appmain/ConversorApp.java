a31531dfaa439c039e310b85597a3a36b8d3b1c9
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-05-08 17:49:33 +0200

    Create ConversorApp.java

---
+++ MVC_ejemplo_akkodis/src/com/ejemplo/appmain/ConversorApp.java
@@ -0,0 +1,33 @@
+package com.ejemplo.appmain;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.ejemplo.controllers.Controlador;
+import com.ejemplo.models.Modelo;
+import com.ejemplo.views.Vista;
+
+public class ConversorApp {
+
+	public static void main(String[] args) {
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO: handle exception
+		}catch (ClassNotFoundException e) {
+			// TODO: handle exception
+		}catch (InstantiationException e) {
+			// TODO: handle exception
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		Modelo modelo = new Modelo();
+		Vista vista = new Vista();
+		Controlador controlador = new Controlador(modelo, vista);
+		controlador.iniciarVista();
+		
+	}
+
+}
