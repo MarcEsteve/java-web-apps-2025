e5c82b502a03bf969ec71f85b429648ccccce2d5
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-05-12 18:08:53 +0200

    Update Contacto.java

--- sesion3-akkodis_web_example_actualizado/src/main/java/com/akkodis/model/Contacto.java
+++ sesion3-akkodis_web_example_actualizado/src/main/java/com/akkodis/model/Contacto.java
@@ -1,16 +1,33 @@
 package com.akkodis.model;
 
 public class Contacto {
-    private String nombre;
-    private String correo;
-    private String mensaje;
+//	Atributos del formulario de contacto
+	private String nombre;
+	private String correo;
+	private String mensaje;
 
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+//	Métodos getters y setters
+	public String getNombre() {
+		return nombre;
+	}
 
-    public String getCorreo() { return correo; }
-    public void setCorreo(String correo) { this.correo = correo; }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    public String getMensaje() { return mensaje; }
-    public void setMensaje(String mensaje) { this.mensaje = mensaje; }
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
 }
