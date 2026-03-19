321c3383465b7cf1eaa83ba31a0515009f76436d
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2026-03-19 13:50:51 +0100

    recuperant repo

--- s01_Proyecto1/src/HolaMundo.java
+++ s01_Proyecto1/src/HolaMundo.java
@@ -4,6 +4,8 @@ public class HolaMundo {
 	public static void main(String[] args) {
 
 		System.out.println("¡Hola, mundo!");
+		System.out.println("¡Bienvenidos a Java!");
+		System.out.println("¡Vamos a aprender a programar Java para Webs!");
 
 	}
 
